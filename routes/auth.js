5e5f3f619fcd296597a74e7137832768770dbfbb
Bojan J <bjosilo@gmail.com>
2021-10-27 21:11:52 -0400

    successful login after decrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+require('dotenv').config();
 
-//REGISTER
+//Register
 router.post('/register', async (req, res) => {
 	const { username, password, email } = req.body;
 
 	const newUser = new User({
 		username,
 		email,
-		password
+		password: CryptoJS.AES.encrypt(password, process.env.SECRET).toString()
 	});
 
 	try {
@@ -20,5 +22,22 @@ router.post('/register', async (req, res) => {
 });
 
 // Login
+router.post('/login', async (req, res) => {
+	try {
+		const user = await User.findOne({ username: req.body.username });
+		!user && res.status(401).json('Wrong credentials');
+
+		const hashedPass = CryptoJS.AES.decrypt(user.password, process.env.SECRET);
+		const pass = hashedPass.toString(CryptoJS.enc.Utf8);
+
+		pass !== req.body.password && res.status(401).json('Wrong credentials');
+
+		const { password, ...otherCredentials } = user._doc;
+
+		res.status(200).json(otherCredentials);
+	} catch (err) {
+		res.json({ message: err.message });
+	}
+});
 
 module.exports = router;
