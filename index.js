20d80af39d35f0e9fe0b3140d794a48949dce66c
Bojan J <bjosilo@gmail.com>
2021-11-04 15:59:23 -0400

    added allowed domains in CORS

--- index.js
+++ index.js
@@ -22,31 +22,32 @@ mongoose
 	.then(() => console.log('Successful connection'))
 	.catch((err) => console.log(err.message));
 
+// CORS ALLOWED DOMAINS
+app.use(function (req, res, next) {
+	const allowedDomains = [
+		'http://localhost:3000',
+		'https://marketplace-josilob.vercel.app'
+	];
+	const origin = req.headers.origin;
+	if (allowedDomains.includes(origin))
+		res.setHeader(('Access-Control-Allow-Origin', origin));
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With,content-type, Accept'
+	);
+	res.setHeader('Access-Control-Allow-Credentials', true);
+
+	next();
+});
+
 // Server routes
 app.get('/', (req, res) => {
 	res.json({ message: 'Marketplace backend home' });
 });
-
-// const whitelist = [
-// 	'localhost:27017',
-// 	'localhost:3000',
-// 	'http://localhost:3000',
-// 	'http://localhost:27017',
-// 	'https://marketplace-neon.vercel.app/',
-// 	'marketplace-neon.vercel.app/'
-// ];
-// const corsOptions = {
-// 	origin: function (origin, callback) {
-// 		if (whitelist.indexOf(origin) !== -1) {
-// 			callback(null, true);
-// 		} else {
-// 			callback(new Error('Not allowed by CORS'));
-// 		}
-// 	},
-// 	methods: ['GET', 'POST', 'PUT', 'DELETE']
-// };
-
-// app.use(cors({ corsOptions }));
 app.use(cors({ origin: '*' }));
 app.use('/users', userRoute);
 app.use('/auth', authRoute);
