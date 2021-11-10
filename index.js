4769aff439794c15105e647513b77cc0dca7e7ea
Bojan J <bjosilo@gmail.com>
2021-11-10 12:29:49 -0500

    cors disabled for testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const cors = require('cors');
+// const cors = require('cors');
 
 // route imports
 const authRoute = require('./routes/auth');
@@ -19,34 +19,36 @@ mongoose
 	.then(() => console.log('Successful connection'))
 	.catch((err) => console.log(err.message));
 
-const headers = (req, res, next) => {
-	const origin = req.headers.origin;
+// const headers = (req, res, next) => {
+// 	const origin = req.headers.origin;
 
-	if (
-		origin == 'http://localhost:3000' ||
-		origin == 'https://marketplace-josilob.vercel.app'
-	)
-		res.setHeader('Access-Control-Allow-Origin', origin);
+// 	if (
+// 		origin == 'http://localhost:3000/' ||
+// 		origin == 'https://marketplace-josilob.vercel.app/'
+// 	)
+// 		res.setHeader('Access-Control-Allow-Origin', origin);
 
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	next();
-};
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+// 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+// 	res.setHeader('Access-Control-Allow-Credentials', true);
+// 	next();
+// };
 
 // use parser for requests
 app.use(express.json()); // parse json bodies
+
+// app.options('*', cors());
 
 // Server routes
 app.get('/', (req, res) => {
 	res.json({ message: 'Marketplace backend home' });
 });
-app.use('/users', headers, userRoute);
-app.use('/auth', headers, authRoute);
-app.use('/products', headers, productRoute);
-app.use('/cart', headers, cartRoute);
-app.use('/orders', headers, orderRoute);
-app.use('/checkout', headers, stripeRoute);
+app.use('/users', userRoute);
+app.use('/auth', authRoute);
+app.use('/products', productRoute);
+app.use('/cart', cartRoute);
+app.use('/orders', orderRoute);
+app.use('/checkout', stripeRoute);
 
 // App listening on port
 app.listen(process.env.PORT || 27017, () =>
